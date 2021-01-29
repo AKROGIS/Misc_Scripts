75fff77835407a3a0c20e9c35f41841e0d42910f
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-01-28 22:23:33 -0900

    Update find_photos.py

--- find_photos.py
+++ find_photos.py
@@ -1,10 +1,22 @@
-import os
-with open(r'C:\tmp\list2.txt', 'w') as outfile:
-    for (root,dirs,files) in os.walk(r'T:\PROJECTS\AKR\FMSS\TRAILS\Data\DENA\2015_FMSSMapping\SavageAlpine'):
-        # print(root)
-        for name in files:
-            ext = name[-4:].lower()
-            # print(ext)
-            if ext == '.jpg' or ext == 'jpeg':
-                outfile.write(root + ',' + name + '\n')
+# -*- coding: utf-8 -*-
 
+"""
+Walk the SEARCH_FOLDER and write the path of any file with an extension in
+EXTENSIONS to the file OUT_LIST.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from io import open
+import os
+
+OUT_LIST = r"C:\tmp\list2.txt"
+SEARCH_FOLDER = r"T:\PROJECTS\AKR\FMSS\TRAILS\Data\DENA\2015_FMSSMapping\SavageAlpine"
+EXTENSIONS = [".jpg", ".jpeg"]
+
+with open(OUT_LIST, "w", encoding="utf-8") as out_file:
+    for (root, _, files) in os.walk(SEARCH_FOLDER):
+        for name in files:
+            _, ext = os.path.splitext(name)
+            if ext.lower() in EXTENSIONS:
+                out_file.write(os.path.join(root, name) + "\n")
