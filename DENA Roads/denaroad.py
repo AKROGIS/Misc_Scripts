63ec0079c1c46e9e9a3c25f0f5c20594ab4b8b7d
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-01-29 17:49:34 -0900

    Move config parameters into config class

--- DENA Roads/denaroad.py
+++ DENA Roads/denaroad.py
@@ -15,70 +15,67 @@ import os
 import sys
 
 
-#############################
-# Start Configuration Options
-#############################
+class Config(object):
+    """Namespace for configuration parameters. Edit as necessary."""
 
-# The file path to the MS Access database of road observations.
-# This was tested with version XXX of MS Access.
-database_path = r"C:\Users\resarwas\Desktop\ProcessingTool_build.mdb"
+    # pylint: disable=useless-object-inheritance,too-few-public-methods
 
-# The connection string to the MS Access database.  In pyodbc format
-# See https://code.google.com/p/pyodbc/wiki/ConnectionStrings
-# The following works on 64bit windows 7 enterprise with MS Access 2010
-connection = r"Driver={Microsoft Access Driver (*.mdb, *.accdb)}; Dbq=" + database_path
-# Neither works on 64bit Windows 8.1 with MS Access 2013
-# connection = r'Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Persist Security Info=False;'.format(database_path)
+    # The file path to the MS Access database of road observations.
+    # This was tested with version XXX of MS Access.
+    database_path = r"C:\Users\resarwas\Desktop\ProcessingTool_build.mdb"
 
-# The name of the measured park road feature class.
-# Could be any valid ArcCatalog path, but the data set must be measured.
-# Only file geodatabase feature classes (not in a feature data set) have been tested.
-road_fc = r"X:\Albers\parks\dena\GeoDB\DENA_Base.gdb\DENA_TRANS_RoadsLocal_ln"
+    # The connection string to the MS Access database.  In pyodbc format
+    # See https://code.google.com/p/pyodbc/wiki/ConnectionStrings
+    # The following works on 64bit windows 7 enterprise with MS Access 2010
+    connection = r"Driver={Microsoft Access Driver (*.mdb, *.accdb)}; Dbq=" + database_path
+    # Neither works on 64bit Windows 8.1 with MS Access 2013
+    # connection = r'Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Persist Security Info=False;'.format(database_path)
 
-# A list of the names of the MS Acccess tables that holds the road observation data
-table_names = [
-    "HikerWaitTime",
-    "OtherStop",
-    "RestStop",
-    "WildlifeNoStop",
-    "WildlifeStop",
-]
+    # The name of the measured park road feature class.
+    # Could be any valid ArcCatalog path, but the data set must be measured.
+    # Only file geodatabase feature classes (not in a feature data set) have been tested.
+    road_fc = r"X:\Albers\parks\dena\GeoDB\DENA_Base.gdb\DENA_TRANS_RoadsLocal_ln"
 
-# This tool assumes that all the tables listed above are in the same database file,
-# And that all the tables have the same 4 columns listed below.
+    # A list of the names of the MS Acccess tables that holds the road observation data
+    table_names = [
+        "HikerWaitTime",
+        "OtherStop",
+        "RestStop",
+        "WildlifeNoStop",
+        "WildlifeStop",
+    ]
 
-# The name of the unique id field in the MS Access table.
-id_field_name = "Point_ID"
+    # This tool assumes that all the tables listed above are in the same database file,
+    # And that all the tables have the same 4 columns listed below.
 
-# The name of the latitude field in the MS Access table. Should be a double
-latitude_field_name = "Latitude"
+    # The name of the unique id field in the MS Access table.
+    id_field_name = "Point_ID"
 
-# The name of the longitude field in the MS Access table. Should be a double
-longitude_field_name = "Longitude"
+    # The name of the latitude field in the MS Access table. Should be a double
+    latitude_field_name = "Latitude"
 
-# The name of the milepost field in the MS Access table. Should be a double
-milepost_field_name = "MilePost"
+    # The name of the longitude field in the MS Access table. Should be a double
+    longitude_field_name = "Longitude"
 
-# This is the ObjectID of the linear feature in the 'road_fc' that is used
-# for linear referencing the mile posts along the road center
-# Must be a number or None.  If it is None, then the first feature in 'road_fc'
-# will be used.
-road_id = 4
+    # The name of the milepost field in the MS Access table. Should be a double
+    milepost_field_name = "MilePost"
 
-# This is the value that is assigned to the starting node of the road feature
-# mile markers are assigned based on the length along the road feature plus
-# the value of the starting mile.
-# The starting mile can be positive or negative.
-road_start_mile = 0
+    # This is the ObjectID of the linear feature in the 'road_fc' that is used
+    # for linear referencing the mile posts along the road center
+    # Must be a number or None.  If it is None, then the first feature in 'road_fc'
+    # will be used.
+    road_id = 4
 
-# This is the search radius for linear referencing points
-# If the point is not within this distance of the park road
-# the point will be not get a milepost.
-search_radius = "100 meters"
+    # This is the value that is assigned to the starting node of the road feature
+    # mile markers are assigned based on the length along the road feature plus
+    # the value of the starting mile.
+    # The starting mile can be positive or negative.
+    road_start_mile = 0
 
-#############################
-# End Configuration Options (do not edit below this line)
-#############################
+    # This is the search radius for linear referencing points
+    # If the point is not within this distance of the park road
+    # the point will be not get a milepost.
+    search_radius = "100 meters"
 
 
 ##############################
@@ -97,10 +94,10 @@ if sys.maxsize > 2 ** 32:
     sys.exit()
 
 # Check for existence of MS Access database
-if not os.path.exists(database_path):
+if not os.path.exists(Config.database_path):
     print(
         "The database does not exist at "
-        + database_path
+        + Config.database_path
         + ". Please check the path, then edit the configuration in "
         + __file__
         + " and try again."
@@ -126,10 +123,10 @@ except ImportError:
     sys.exit()
 
 # Try finding the road feature class
-if not arcpy.Exists(road_fc):
+if not arcpy.Exists(Config.road_fc):
     print(
         "Could not find the roads at "
-        + road_fc
+        + Config.road_fc
         + ". Please check the path, then edit the configuration in "
         + __file__
         + " and try again."
@@ -140,18 +137,18 @@ if not arcpy.Exists(road_fc):
 print("Getting Measured Road Centerline...")
 road = None
 where = None
-if road_id:
-    where = "{} = {}".format(arcpy.Describe(road_fc).OIDFieldName, road_id)
-with arcpy.da.SearchCursor(road_fc, "Shape@", where) as cursor:
+if Config.road_id:
+    where = "{} = {}".format(arcpy.Describe(Config.road_fc).OIDFieldName, Config.road_id)
+with arcpy.da.SearchCursor(Config.road_fc, "Shape@", where) as cursor:
     row = cursor.next()
     if row:
         road = row[0]
 
 if not road:
     id = "first"
-    if road_id:
-        id = "oid = {0}".format(road_id)
-    print("The road feature was not found ({0} in {1})".format(id, road_fc))
+    if Config.road_id:
+        id = "oid = {0}".format(Config.road_id)
+    print("The road feature was not found ({0} in {1})".format(id, Config.road_fc))
     sys.exit()
 
 ############################
@@ -173,7 +170,7 @@ def calculate(pts, line, start=0, search_radius="100 Meters"):
     with arcpy.da.InsertCursor(pt_fc, ["OBS_ID", "SHAPE@XY"]) as in_cursor:
         for pt in pts:
             in_cursor.insertRow([pt[0], (pt[2], pt[1])])
-    arcpy.LocateFeaturesAlongRoutes_lr(pt_fc, road_fc, rid, search_radius, tbl, props)
+    arcpy.LocateFeaturesAlongRoutes_lr(pt_fc, Config.road_fc, rid, search_radius, tbl, props)
     with arcpy.da.SearchCursor("in_memory\\results", ["OBS_ID", "MP"]) as out_cursor:
         mileposts = dict(out_cursor)
     if start != 0:
@@ -184,7 +181,7 @@ def calculate(pts, line, start=0, search_radius="100 Meters"):
     return mileposts
 
 
-for table_name in table_names:
+for table_name in Config.table_names:
     print("Processing Table {0} ...".format(table_name))
     # Get records that need to be updated
     print("  Finding Road Observations to Update...")
@@ -193,34 +190,34 @@ for table_name in table_names:
         + "WHERE {1} IS NULL AND {2} IS NOT NULL AND {3} IS NOT NULL"
     )
     sql = sql.format(
-        id_field_name,
-        milepost_field_name,
-        latitude_field_name,
-        longitude_field_name,
+        Config.id_field_name,
+        Config.milepost_field_name,
+        Config.latitude_field_name,
+        Config.longitude_field_name,
         table_name,
     )
 
     rcursor = None
     try:
-        rcursor = pyodbc.connect(connection).cursor()
+        rcursor = pyodbc.connect(Config.connection).cursor()
         pass
     except pyodbc.Error as e:
         print("Rats!!  Unable to connect to your database.")
         print("Contact Regan (regan_sarwas@nps.gov) for assistance.")
-        print("  Connection: " + connection)
+        print("  Connection: " + Config.connection)
         print("  Error: " + e[1])
         sys.exit()
     rows = rcursor.execute(sql).fetchall()
 
     print("  Calculating {0} Road Observations".format(len(rows)))
-    mileposts = calculate(rows, road, road_start_mile, search_radius)
+    mileposts = calculate(rows, road, Config.road_start_mile, Config.search_radius)
 
     print("\n  Updating Database...")
-    wcursor = pyodbc.connect(connection).cursor()
+    wcursor = pyodbc.connect(Config.connection).cursor()
     for id in mileposts:
         sql = "UPDATE {0} SET {1} = {2} WHERE {3} = {4}"
         sql = sql.format(
-            table_name, milepost_field_name, mileposts[id], id_field_name, id
+            table_name, Config.milepost_field_name, mileposts[id], Config.id_field_name, id
         )
         # print(sql)
         wcursor.execute(sql)
