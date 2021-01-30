4660157956676fa59ed0c925ae11fbe300355ff8
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-01-30 11:35:24 -0900

    Move code out of global scope

--- attributes_of_photo_files.py
+++ attributes_of_photo_files.py
@@ -32,81 +32,87 @@ class Config(object):
     # Write the list of file data to this file.
     out_list = os.path.join(search_folder, "PhotoList.csv")
 
+def write_exif():
+    """Write a list of EXIF properties for all files in Config."""
 
-with open(Config.out_list, "w", encoding="utf-8") as f:
-    f.write("folder,photo,exifdate,lat,lon,gpsdate,filedate\n")
-    for filename in os.listdir(Config.search_folder):
-        base, extension = os.path.splitext(filename)
-        if extension.lower() == ".jpg":
-            path = os.path.join(Config.search_folder, filename)
-            lat, lon, exifdate, gpsdate = "", "", "", ""
-            with open(path, "rb") as pf:
-                # exifread wants binary data
-                tags = exifread.process_file(pf, details=False)
-                try:
-                    dms = tags["GPS GPSLatitude"].values
-                    deg = float(dms[0].num) / dms[0].den
-                    minute = float(dms[1].num) / dms[1].den
-                    sec = float(dms[2].num) / dms[2].den
-                    if tags["GPS GPSLatitudeRef"].values == "N":
-                        sign = 1
-                    else:
-                        sign = -1
-                    lat = sign * (deg + (minute + sec / 60) / 60)
-                    if lat == 0:
-                        lat = ""
-                except KeyError:
-                    pass
-                except ZeroDivisionError:
-                    pass
-                try:
-                    dms = tags["GPS GPSLongitude"].values
-                    deg = float(dms[0].num) / dms[0].den
-                    minute = float(dms[1].num) / dms[1].den
-                    sec = float(dms[2].num) / dms[2].den
-                    if tags["GPS GPSLongitudeRef"].values == "E":
-                        sign = 1
-                    else:
-                        sign = -1
-                    lon = sign * (deg + (minute + sec / 60) / 60)
-                    if lon == 0:
-                        lon = ""
-                except KeyError:
-                    pass
-                except ZeroDivisionError:
-                    pass
-                try:
-                    time = tags["EXIF DateTimeOriginal"].values
-                    # exifdate = time.replace(':', '').replace(' ', 'T') #compact iso format
-                    exifdate = time.replace(
-                        ":", "-", 2
-                    )  # microsoft excel acceptable ISO format
-                except KeyError:
-                    pass
-                try:
-                    date = tags["GPS GPSDate"].values
-                    time = tags["GPS GPSTimeStamp"].values
-                    if date:
-                        # gpsdate = '{0}T{1}{2}{3}'.format(date.replace(':', ''),
-                        #                                 time[0], time[1], float(time[2].num)/time[2].den)
-                        gpsdate = "{0} {1}:{2}:{3}".format(
-                            date.replace(":", "-"),
-                            time[0],
-                            time[1],
-                            float(time[2].num) / time[2].den,
-                        )
-                    else:
-                        gpsdate = ""
-                except KeyError:
-                    pass
-            filedate = datetime.datetime.fromtimestamp(
-                os.path.getmtime(path)
-            ).isoformat()
-            filedate = filedate.replace(
-                "T", " "
-            )  # microsoft excel acceptable ISO format
-            f.write(
-                "{0},{1},{2},{3},{4},{5},{6}\n".format(
-                    Config.search_folder, filename, exifdate, lat, lon, gpsdate, filedate
+    with open(Config.out_list, "w", encoding="utf-8") as f:
+        f.write("folder,photo,exifdate,lat,lon,gpsdate,filedate\n")
+        for filename in os.listdir(Config.search_folder):
+            base, extension = os.path.splitext(filename)
+            if extension.lower() == ".jpg":
+                path = os.path.join(Config.search_folder, filename)
+                lat, lon, exifdate, gpsdate = "", "", "", ""
+                with open(path, "rb") as pf:
+                    # exifread wants binary data
+                    tags = exifread.process_file(pf, details=False)
+                    try:
+                        dms = tags["GPS GPSLatitude"].values
+                        deg = float(dms[0].num) / dms[0].den
+                        minute = float(dms[1].num) / dms[1].den
+                        sec = float(dms[2].num) / dms[2].den
+                        if tags["GPS GPSLatitudeRef"].values == "N":
+                            sign = 1
+                        else:
+                            sign = -1
+                        lat = sign * (deg + (minute + sec / 60) / 60)
+                        if lat == 0:
+                            lat = ""
+                    except KeyError:
+                        pass
+                    except ZeroDivisionError:
+                        pass
+                    try:
+                        dms = tags["GPS GPSLongitude"].values
+                        deg = float(dms[0].num) / dms[0].den
+                        minute = float(dms[1].num) / dms[1].den
+                        sec = float(dms[2].num) / dms[2].den
+                        if tags["GPS GPSLongitudeRef"].values == "E":
+                            sign = 1
+                        else:
+                            sign = -1
+                        lon = sign * (deg + (minute + sec / 60) / 60)
+                        if lon == 0:
+                            lon = ""
+                    except KeyError:
+                        pass
+                    except ZeroDivisionError:
+                        pass
+                    try:
+                        time = tags["EXIF DateTimeOriginal"].values
+                        # exifdate = time.replace(':', '').replace(' ', 'T') #compact iso format
+                        exifdate = time.replace(
+                            ":", "-", 2
+                        )  # microsoft excel acceptable ISO format
+                    except KeyError:
+                        pass
+                    try:
+                        date = tags["GPS GPSDate"].values
+                        time = tags["GPS GPSTimeStamp"].values
+                        if date:
+                            # gpsdate = '{0}T{1}{2}{3}'.format(date.replace(':', ''),
+                            #                                 time[0], time[1], float(time[2].num)/time[2].den)
+                            gpsdate = "{0} {1}:{2}:{3}".format(
+                                date.replace(":", "-"),
+                                time[0],
+                                time[1],
+                                float(time[2].num) / time[2].den,
+                            )
+                        else:
+                            gpsdate = ""
+                    except KeyError:
+                        pass
+                filedate = datetime.datetime.fromtimestamp(
+                    os.path.getmtime(path)
+                ).isoformat()
+                filedate = filedate.replace(
+                    "T", " "
+                )  # microsoft excel acceptable ISO format
+                f.write(
+                    "{0},{1},{2},{3},{4},{5},{6}\n".format(
+                        Config.search_folder, filename, exifdate, lat, lon, gpsdate, filedate
+                    )
                 )
-            )
+
+
+if __name__ == "__main__":
+    write_exif()
--- metadata_keyword_values.py
+++ metadata_keyword_values.py
@@ -22,23 +22,29 @@ class Config(object):
     ORI = r"\\INPAKROVMDIST\GISData2\Extras\AKR\Statewide\DEM\NPRA-IfSAR_Intermap_2002\ORI"
     prefix = "        Beginning_Date: "
 
-for folder in [Config.DSM, Config.DTM, Config.ORI]:
-    d = {}
-    for name in os.listdir(folder):
-        path = os.path.join(folder, name)
-        ext = os.path.splitext(path)[1].lower()
-        if os.path.isfile(path) and ext == ".txt":
-            date = "none"
-            with open(path, "r", encoding="utf-8") as f:
-                for line in f:
-                    if line.startswith(Config.prefix):
-                        date = line.replace(Config.prefix, "").strip()
-                        break
-            # print(date)
-            if date not in d:
-                d[date] = 0
-            d[date] = d[date] + 1
-    print("Stats for " + folder[-3:])
-    keys = sorted(d.keys())
-    for key in keys:
-        print("  " + key + ":" + str(d[key]))
+def find_metadata():
+    """Find all the metadata values matching the Config parameters."""
+
+    for folder in [Config.DSM, Config.DTM, Config.ORI]:
+        dates = {}
+        for name in os.listdir(folder):
+            path = os.path.join(folder, name)
+            ext = os.path.splitext(path)[1].lower()
+            if os.path.isfile(path) and ext == ".txt":
+                date = "none"
+                with open(path, "r", encoding="utf-8") as in_file:
+                    for line in in_file:
+                        if line.startswith(Config.prefix):
+                            date = line.replace(Config.prefix, "").strip()
+                            break
+                # print(date)
+                if date not in dates:
+                    dates[date] = 0
+                dates[date] = dates[date] + 1
+        print("Stats for {0}".format(folder[-3:]))
+        for key in sorted(dates.keys()):
+            print("  {0}:{1}".format(key,dates[key]))
+
+
+if __name__ == "__main__":
+    find_metadata()
